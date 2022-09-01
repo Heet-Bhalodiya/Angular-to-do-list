b25f72f8c27cd5b5268a395c1139eedb5749565a
Heet Bhalodiya <100203834+Heet-Bhalodiya@users.noreply.github.com>
2022-09-01 22:56:46 +0530

    Add files via upload

---
+++ Todo.ts
@@ -0,0 +1,24 @@
+export interface Todo{
+    sno: number 
+    title: string
+    desc: string
+    active: boolean
+}
+
+// export class Todo{
+//     sno: number 
+//     title: string
+//     desc: string
+//     active: boolean
+
+//     constructor(sno: number, title: string, desc: string, active: boolean) {
+//         this.sno = sno;
+//         this.title = title;
+//         this.desc = desc;
+//         this.active = active;
+//     }
+// }   
+
+// this.todos = [
+//     new Todo(1, "Title1", "description1", true),
+// ];
